c82f2963f2ea4e1eb8bc3f2b47b8fcf34bdd9149
Gambor27 <gambor27@gmail.com>
2024-03-16 15:48:49 -0700

    Finished

--- report.js
+++ report.js
@@ -1,9 +1,17 @@
+const fs = require('fs')
+
+
 function printReport(pages) {
     console.log("Writing Report To File...")
-    //sort
-    for (page in pages) {
-        writeFile("./report.txt", `${page} found ${pages[page]} times`, {flag: a})
+    let report = ""
+    const sortedPages = Object.entries(pages).sort((a, b) => b[1] - a[1])
+    for (page in sortedPages) {
+        report += `${page} found ${sortedPages[page]} times \n`
     }
+    console.log(report)
+    fs.writeFile("./report.txt", report, err => {
+        if (err) throw err
+    })
     console.log("Report Complete")
 }
 
