c679d5161c00b836d391606bb3722ab5704f7f06
Gambor27 <gambor27@gmail.com>
2024-03-16 12:07:05 -0700

    Extracts Link URL

--- crawl.js
+++ crawl.js
@@ -11,6 +11,21 @@ function normalizeURL(url){
     }
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+    const page = new JSDOM(htmlBody)
+    const aTags = page.window.document.querySelectorAll('a')
+    const tagStrings = Array.from(aTags).map(tag => tag.href)
+    const outputStrings = []
+    for (let tag of tagStrings) {
+        if (tag.startsWith('/')) {
+            outputStrings.push(`${baseURL}${tag}`)
+        } else {
+            outputStrings.push(tag)
+        }
+    }
+    return outputStrings
+}
+
 module.exports = {
-    normalizeURL
+    normalizeURL, getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,6 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl')
+const {getURLsFromHTML } = require('./crawl')
 
 test ('format https://url.com/path/ correctly', () => {
     expect(normalizeURL('https://url.com/path/')).toBe('url.com/path')
@@ -13,8 +14,8 @@ test ('format http://URL.com/path/ correctly', () => {
     expect(normalizeURL('http://URL.com/path/')).toBe('url.com/path')
 })
 
-test ('format http://url.com/path/ correctly', () => {
-    expect(normalizeURL('http://url.com/path')).toBe('url.com/path')
+test ('format https://url.com/path/ correctly', () => {
+    expect(normalizeURL('https://url.com/path')).toBe('url.com/path')
 })
 
 test ('format \'https://url.com/path/ \' correctly', () => {
@@ -23,4 +24,20 @@ test ('format \'https://url.com/path/ \' correctly', () => {
 
 test ('format https://url.com/ correctly', () => {
     expect(normalizeURL('https://url.com/')).toBe('url.com')
+})
+
+test ('find URL in <a href="https://boot.dev">Learn</a>', () => {
+    expect(getURLsFromHTML('<a href="https://boot.dev">Learn</a>', 'https://boot.dev')).toEqual(['https://boot.dev/'])
+})
+
+test ('find URL in <a href="./boot">Learn</a>', () => {
+    expect(getURLsFromHTML('<a href="/boot">Learn</a>', 'https://boot.dev')).toEqual(['https://boot.dev/boot'])
+})
+
+test ('find URL in <a href="https://boot.dev/boot">Learn</a>', () => {
+    expect(getURLsFromHTML('<a href="https://boot.dev/boot">Learn</a>', 'https://boot.dev')).toEqual(['https://boot.dev/boot'])
+})
+
+test ('find URL in <a href="https://boot.dev/boot">Learn</a><a href="https://boot.dev">Learn</a>', () => {
+    expect(getURLsFromHTML('<a href="https://boot.dev/boot">Learn</a><a href="https://boot.dev">Learn</a>', 'https://boot.dev')).toEqual(['https://boot.dev/boot','https://boot.dev/'])
 })
